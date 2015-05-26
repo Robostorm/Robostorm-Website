78383dabaafa00f988989834eeeb1af2830aa493
Tim Hollabaugh <haytowntim@embarqmail.com>
2015-05-25 20:14:12 -0400

    Initial Commit

---
+++ header.php
@@ -0,0 +1,35 @@
+<!-- -*-HTML-*- -->
+<nav class="navbar navbar-static-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">
+        <img alt="" src="images/banner.png">
+      </a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li>
+	  <a href="projects.php" class="dropdown-toggle" data-toggle="dropdown" role="botton" aria-expanded="false">Projects <span class="caret"></span></a>
+	  <ul class="dropdown-menu" role="menu">
+	    <li><a href="aquabot.php">Aquabot</a></li>
+	    <li><a href="battlebots.php">BattleBots</a></li>
+	    <li><a href="mazebots.php">MazeBots</a></li>
+	    <li><a href="3dprinter.php">3d Printer</a></li>
+	    <li><a href="arcade.php">Arcade Machine</a></li>
+	  </ul>
+	</li>
+        <li><a href="members.php">Members</a></li>
+	<li><a href="documents.php">Documents</a></li>
+	<li><a href="events.php">Events</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>      
+    </div>
+  </div>
+</nav>
