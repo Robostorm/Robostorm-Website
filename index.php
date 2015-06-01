96194725f2275d7a605a6b7cb45d6de3e58d913c
zethra <jediben97@gmail.com>
2015-05-31 20:03:38 -0400

    Chnaged Images folder to assets and move footer file ton index file

--- index.php
+++ index.php
@@ -39,12 +39,39 @@
 	  </p>
 	</div>
 	<div class="col-md-4">
-	  <img src="images/turretweb.jpg">
+	  <img src="assets/turretweb.jpg">
 	</div>
       </div>
     </div>
     
-    <?php include 'footer.php'; ?>
+    <footer>
+	  <div id="rutgers">
+		<a href="https://search.rutgers.edu/"><img src="assets/webbanner.jpg" alt="Rutgers" width="100%" border="0"></a>
+	  </div>
+	  <div class="text-center">
+		<img class="clover" src="assets/4H_clover.png" alt="4H Clover">
+		<div style="display: inline-block">
+		  <p>
+		<b>
+		  Rutgers Cooperative Extention<br>
+		  4-H of Hunterdon County<br>
+		  <br>
+		  314 State Route 12, Building #2<br>
+		  P.O. Box 2900<br>
+		  Flemington NJ 08822<br>
+		  (908) 788-1341<br>
+		  <a href="mailto:hunterdon4h@co.hunterdon.nj.us" class="op">hunterdon4h@co.hunterdon.nj.us</a><br>
+		  <a href="http://www.co.hunterdon.nj.us/rutgers/4H.htm" target="_blank" class="op">http://www.co.hunterdon.nj.us/rutgers/4H.htm</a>
+		</b>
+		  </p>
+		</div>
+		<img class="clover"src="assets/4H_clover.png" alt="4H Clover">
+	  </div>
+	  <div>
+		<p align="center"><b>The 4-H Youth Development Program is part of Rutgers Cooperative Extension. 4-H offers educational programs to all youth, grades K-13, on an age appropriate basis, without regard to race, religion, color, national origin, gender, sexual orientation or disability. For additional information, contact their website at <a href="http://www.nj4h.rutgers.edu/" target="_blank" class="op">http://www.nj4h.rutgers.edu/</a></b></p>
+	  </div>
+	  <p><b>Last updated: 5/25/2015</b></p>
+	</footer>
     
   </body>
 </html>
