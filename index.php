10475dadff1292ace54deae6ace2093e15325eb0
zethra <jediben97@gmail.com>
2015-06-22 20:09:09 -0400

    Minor tweks

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
-  <?php include 'includes.php'; ?>
+  <head>
+	  <title>Robostorm</title>
+	  <?php require 'includes.php'; ?>
+  </head>
 
   <body>
     
@@ -12,19 +14,19 @@
 	<div class="col-md-8">
 	  <h2>Welcome to Robostorm</h2>
 	  <p>
-	    Here we inspire young minds to think outside the box about technology. Everyone is given the chance to take part in any of our many projects and recive advice from other members. Every project is member run, and we try to make sure that every member has an equal chance to participate.
+	    Here we inspire young minds to think outside the box about technology. Everyone is given the chance to take part in any of our many projects and receive advice from other members. Every project is member run, and we try to make sure that every member has an equal chance to participate.
 	  </p>
 	  <p>
 	    Our club started in 2011. We had only four members at the first meeting, and we have grown quickly since. We are now a club of 15+ members, and we are looking to gain more in the fall. Because we were so small the first year, we only had a display case in the Rasmburg Building, and showed off our projects for a couple hours a day in the sheep tent. Now, we have a small shed and a 40ft x 40ft tent to house our club at the fair.
 	  </p>
 	  <p>
-	    Every year, we continue work on our perenial projects. These projects get improved from year to year, and are at every fair. The most noticable of these is the Aquabot, a robot that we drive aound the fair ground to sell water. Another project that we just added this year was the 3d printer. This will be at the fair every year, along with club members's 3d printers (Currently about five members have their own 3d printers). By continuously improving these projects, the club can explore new technologies to experiment of a platform that is already built. This gives the members a chance to try new ideas without building from scratch.
+	    Every year, we continue work on our personal projects. These projects get improved from year to year, and are at every fair. The most notable of these is the Aquabot, a robot that we drive around the fair ground to sell water. Another project that we just added this year was the 3d printer. This will be at the fair every year, along with club members's 3d printers (Currently about five members have their own 3d printers). By continuously improving these projects, the club can explore new technologies to experiment of a platform that is already built. This gives the members a chance to try new ideas without building from scratch.
 	  </p>
 	  <p>
-	    In addition to the the perenial projects that stay from year to year, the club tries to work on some prohjects that will stay only for the current year, or maybe stick around for the next year. The gives members a chance to start fresh on something new, not worring about previous work. Last year, the club built an Arcade Machine to have some fun with a Raspberry Pi. THe Arcade Machine will also likly be around for this year as well.
+	    In addition to the the personal projects that stay from year to year, the club tries to work on some projects that will stay only for the current year, or maybe stick around for the next year. The gives members a chance to start fresh on something new, not working about previous work. Last year, the club built an Arcade Machine to have some fun with a Raspberry Pi. THe Arcade Machine will also likely be around for this year as well.
 	  </p>
 	  <p>
-	    Furthermore, each club member is required to complete a personal project of some kind to show off at the fair. This could be anything from a simple Lego Mindstorms robot to an autonomous multirotor. This give each kid the chance to develop their own ideas, independent of other members. Although these are independant projects, every member is willing to help out others in times of difficulty. Each members creates a poster to go with their project to explain to visitors what their project does and how it works. 
+	    Furthermore, each club member is required to complete a personal project of some kind to show off at the fair. This could be anything from a simple Lego Mindstorms robot to an autonomous multirotor. This give each kid the chance to develop their own ideas, independent of other members. Although these are independent projects, every member is willing to help out others in times of difficulty. Each members creates a poster to go with their project to explain to visitors what their project does and how it works. 
 	  </p>
 	  <p>
 	    There have been other robotics clubs in Hunterdon County that have come and gone since we started. Many of them have not gained enough full time members to stay around for more than a few years.
