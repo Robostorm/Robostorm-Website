c3cb1348d1fe85942fc295b749d3e908f968dd49
zethra <jediben97@gmail.com>
2015-06-22 22:47:06 -0400

    Moved css file to css directory and javascript file to js directory

--- includes.php
+++ includes.php
@@ -2,8 +2,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" type="image/png" href="assets/icon.png">     
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="css/styles.css">
 <link href='http://fonts.googleapis.com/css?family=Ubuntu:400,700|Ubuntu+Mono:400,700' rel='stylesheet' type='text/css'>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<script src="script.js"></script>
+<script src="js/script.js"></script>
